5830ee4c74dc0527130c1d467ba2105e9d2ddc97
Janet <janetkarpenske@gmail.com>
2020-06-04 15:54:07 -0700

    Updated calculator to allow user to select which math expression they would like to use

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,21 +1,19 @@
-const add = function(number1, number2) {
-    return number1 + number2;
-  };
-  
-  const subtract = function(number1, number2) {
-      return number1 - number2;
-  };
-  
-  const multiply = function(number1, number2) {
-      return number1 * number2;
-  };
-  
-  const divide = function(number1, number2) {
-      return number1 / number2;
-  };
-  
-  const number1 = parseInt(prompt("Enter a number:"));
-  const number2 = parseInt(prompt("Enter another number:"));
-  
-  const result = divide(number1, number2);
-  alert(result);
+$(document).ready(function() {
+  $("form#calculator").submit(function() {
+    event.preventDefault();
+    const number1 = parseInt($("#input1").val());
+    const number2 = parseInt($("#input2").val());
+    const operator = $("input:radio[name=operator]:checked").val();
+    const result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
+  });
+});
